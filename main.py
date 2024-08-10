a7890914a298ce78aae8d919d6aabe4f654dd5d3
Minidolmer <59773666+minidolmer@users.noreply.github.com>
2024-08-10 12:08:32 +0200

    Added collisions and manual jumping

--- main.py
+++ main.py
@@ -19,14 +19,33 @@ moving_left = False
 
 player_location = [150,150]
 player_y_momentum = 0
+grounded = False #Am i on the ground?
+jump_count = 0
+box_location = [200, 300]
+box_size = [100, 20]
 
 while True:
     screen.fill((150,250,250))
-    
+    pygame.draw.rect(screen, (29, 207, 195), (*box_location, *box_size))
     screen.blit(player_image,player_location)
+    player_rect = pygame.Rect(player_location[0], player_location[1], player_image.get_width(), player_image.get_height())
+    box_rect = pygame.Rect(box_location[0], box_location[1], box_size[0], box_size[1])
     
-    if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
-        player_y_momentum = -player_y_momentum
+    if player_rect.colliderect(box_rect) and player_y_momentum > 0:
+        player_location[1] = box_location[1] - player_image.get_height()
+        player_y_momentum = 0
+    
+    if player_rect.colliderect(box_rect) and player_y_momentum > 0:
+        player_location[1] = box_location[1] - player_image.get_height()
+        player_y_momentum = 0
+        grounded = True
+        jump_count = 0
+    
+    if player_location[1] > WINDOW_SIZE[1] - player_image.get_height():
+        player_location[1] = WINDOW_SIZE[1] - player_image.get_height()
+        player_y_momentum = 0
+        grounded = True
+        jump_count = 0
     else:
         player_y_momentum += 0.2
     player_location[1] += player_y_momentum
@@ -43,6 +62,11 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == KEYDOWN:
+            if event.key == K_SPACE and (grounded or jump_count < 2):
+                player_y_momentum = -10
+                grounded = False
+                jump_count += 1
+                print("jumping :D")
             if event.key == K_d:
                 moving_right = True
                 print("moving right")
